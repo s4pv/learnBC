b1ad6e24bda58ea2bdb479d8a30b7b4cce9fb899
s4pv <pablo.sapunarh@gmail.com>
2021-12-16 19:02:05 -0300

    Added persistence

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,44 @@
 package blockchain
 
-type BlockChain struct {
-    Blocks []*Block
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
+func Handle(err error) {
+    if err != nil {
+        log.Panic(err)
+    }
 }
+
+func (b *Block) Serialize() []byte {
+    var res bytes.Buffer
+    encoder := gob.NewEncoder(&res)
+
+    err := encoder.Encode(b)
+
+    Handle(err)
+
+    return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+    var block Block
+
+    decoder := gob.NewDecoder(bytes.NewReader(data))
+
+    err := decoder.Decode(&block)
+
+    Handle(err)
+
+    return &block
+}
+
+//type BlockChain struct {
+//    Blocks []*Block
+
+//}
 
 type Block struct {
     Hash     []byte
@@ -24,18 +59,18 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-    prevBlock := chain.Blocks[len(chain.Blocks)-1]
-    new := CreateBlock(data, prevBlock.Hash)
-    chain.Blocks = append(chain.Blocks, new)
+//func (chain *BlockChain) AddBlock(data string) {
+//    prevBlock := chain.Blocks[len(chain.Blocks)-1]
+//    new := CreateBlock(data, prevBlock.Hash)
+//    chain.Blocks = append(chain.Blocks, new)
 
-}
+//}
 
 func Genesis() *Block {
     return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain {
-    return &BlockChain{[]*Block{Genesis()}}
+//func InitBlockChain() *BlockChain {
+//    return &BlockChain{[]*Block{Genesis()}}
 
-}
+//}
--- main.go
+++ main.go
@@ -1,29 +1,96 @@
 package main
 
 import (
+    "flag"
     "fmt"
+    "os"
+    "runtime"
     "strconv"
+
 	"github.com/s4pv/learnBC/blockchain"
 
 )
 
+type CommandLine struct {
+    blockchain *blockchain.BlockChain
+}
+
+func (cli *CommandLine) printusage() {
+    fmt.Println("Usage: ")
+    fmt.Println(" add -block <BLOCK_DATA> - add a block to the chain")
+    fmt.Println(" print - prints the blocks in the chain")
+}
+
+func (cli *CommandLine) validateArgs() {
+    if len(os.Args) < 2 {
+        cli.printusage()
+        runtime.Goexit()
+
+    }
+}
+
+func (cli *CommandLine) printChain() {
+    iterator := cli.blockchain.Iterator()
+
+    for {
+        block := iterator.Next()
+        fmt.printf("Previous hash: %x\n", block.PrevHash)
+        fmt.printf("data: %s\n", block.Data)
+        fmt.printf("hash: %x\n", block.Hash)
+        pow := blockchain.NewProofOfWork(block)
+        fmt.printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+        fmt.println()
+
+        if len(block.PrevHash) == 0 {
+            break
+        }
+    }
+}
+
+
+func (cli *CommandLine) run() {
+    cli.validateArgs()
+
+    addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+    printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+    addBlockData := addBlockCmd.String("block", "", "Block data")
+
+    switch os.Args[1] {
+    case "add":
+        err := addBlockCmd.Parse(os.Args[2:])
+        blockchain.Handle(err)
+
+    case "print":
+        err := printChainCmd.Parse(os.Args[2:])
+        blockchain.Handle(err)
+
+    default:
+        cli.printusage()
+        runtime.Goexit()
+
+    }
+
+    if addBlockCmd.Parsed() {
+        if *addBlockData == "" {
+            addBlockCmd.Usage()
+            runtime.Goexit()
+        }
+        cli.addBlock(*addBlockData)
+    }
+    if printChainCmd.Parsed() {
+        cli.printChain()
+    }
+
+}
+
 func main() {
+    defer os.Exit(0)
 
     chain := blockchain.InitBlockChain()
+    defer chain.Database.Close()
 
-    chain.AddBlock("first block after genesis")
-    chain.AddBlock("second block after genesis")
-    chain.AddBlock("third block after genesis")
+    cli := CommandLine{chain}
 
-    for _, block := range chain.Blocks {
-
-        fmt.Printf("Previous hash: %x\n", block.PrevHash)
-        fmt.Printf("data: %s\n", block.Data)
-        fmt.Printf("hash: %x\n", block.Hash)
-
-        pow := blockchain.NewProofOfWork(block)
-        fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
-        fmt.Println()
-    }
+    cli.run()
 
 }
